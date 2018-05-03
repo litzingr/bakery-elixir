f3bb0e96db205b462f9bc4c1c92485610a0b2aa7
Zach Litzinger <litzi005@morris.umn.edu>
2018-05-03 13:16:40 -0500

    initialize mix project

---
+++ lib/bakery.ex
@@ -0,0 +1,18 @@
+defmodule Bakery do
+  @moduledoc """
+  Documentation for Bakery.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bakery.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bakery_test.exs
@@ -0,0 +1,8 @@
+defmodule BakeryTest do
+  use ExUnit.Case
+  doctest Bakery
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
